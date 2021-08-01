3bfce9814c0a8003a9eb9b947571c5bd2af23e32
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-08-01 19:10:06 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -27,7 +27,7 @@ class Config(object):
     SCREENSHOTS = os.environ.get("SCREENSHOTS", "True")
 
     # default thumbnail to be used in the videos
-    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
+    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", None)
 
     # proxy for accessing youtube-dl in GeoRestricted Areas
     # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
